cf22e7a0f3426f3760dd678ee2807d921e9e2dfd
Pun Kongsomboon <ksm_punn@hotmail.com>
2022-10-04 03:05:28 +0700

    inital commit

---
+++ src/script_page/login.js
@@ -0,0 +1,32 @@
+
+
+export default {
+    // setup(props) {
+
+    // },
+    data() {
+        return {
+
+        }
+    },
+    beforeCreate() {
+        // document.querySelector('body').setAttribute('style', 'background: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);')
+    },
+    // beforeRouteEnter (to, from, next) {
+    //     // ...
+    // },
+    component: {
+
+    },
+    beforeCreate(){
+        
+    },
+    created() {
+        console.log("test");
+    },
+    methods: {
+        
+    },
+
+}
+
---
+++ src/views/login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row text-center align-items-center" style="height: 100vh;">
+
+            <div class="col-12 d-flex justify-content-center">
+                <div class="col-4" style="height: 500px; border:  2px solid red;">
+
+                    <h1 class="">test</h1>
+                </div>
+                <div class="col-4" style="height: 500px; border:  2px solid red; ">
+
+                    <h1 class="">test</h1>
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+</template>
+
+<script src="@/script_page/login.js"></script>
+
+<style scoped>
+@import '@/assets/css/login.css';
+</style>
